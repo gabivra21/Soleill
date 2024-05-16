3482d0e1b17cd738676c053f8daf514e4432e6f3
Letícia Aiko <131884917+letaiko@users.noreply.github.com>
2024-05-16 12:35:02 -0300

    Update Pedido.java

--- src/Pedido.java
+++ src/Pedido.java
@@ -2,5 +2,5 @@ public class Pedido {
   private int prazoEntrega;
   private
   private 
-  private String endEntrega;
+  private String endEntrega; //ops coloquei o commit no lugar errado, depois arrumo
 }
